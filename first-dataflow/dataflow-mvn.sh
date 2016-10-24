b0c8dfff5e1bfdbc782c1f4497b145acfb15bde0
Bogdan State <bogdanstate@gmail.com>
2016-10-24 16:16:12 -0700

    Moving directories

--- first-dataflow/dataflow-mvn.sh
+++
@@ -1,9 +0,0 @@
- mvn compile exec:java \
-      -Dexec.mainClass=com.google.cloud.dataflow.examples.WordCount \
-      -Dexec.args="--project=social-norms \
-      --output=my_output.txt \
-      --inputFile=/home/bogdan/Downloads/Wikipedia-20161021025047.xml"
- 
-#--output=gs://social-norms/output \
-#--stagingLocation=gs://social-norms/staging/ \
-#--runner=BlockingDataflowPipelineRunner"
